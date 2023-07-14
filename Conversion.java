4f53774e85ec9eaadacc902d3633aa2e7aaed60d
ShraddhaShravanNikam <137033101+ShraddhaShravanNikam@users.noreply.github.com>
2023-07-14 08:59:19 -0700

    Conversion

---
+++ Conversion.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Conversion
+{
+  public static void main(String arg[])
+{
+   String g ="100";
+   int i=Integer.parseInt(g);
+   System.out.println("VAlue ="+i);
+}
+}
