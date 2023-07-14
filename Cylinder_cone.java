2d84b5314bb09462631951e9a722873a40b96a6a
ShraddhaShravanNikam <137033101+ShraddhaShravanNikam@users.noreply.github.com>
2023-07-14 08:59:47 -0700

    Add files via upload

---
+++ Cylinder_cone.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Cylinder_cone extends Applet
+{
+    
+    public void paint(Graphics g)
+{
+     g.drawOval(500,60,200,50);
+     g.drawOval(500,280,200,50);
+    g.drawLine(500,80,500,300);
+     g.drawLine(700,80,700,300);
+    }
+} 
+/* <applet code="Cylinder_cone .class" height=2000 width=2000> 
+</applet> */
